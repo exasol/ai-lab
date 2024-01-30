abcbbdd68d2719cf0e8d9d30e5f06d8a049f82be
mibe <mikhail.beck@exasol.com>
2024-01-30 08:28:04 +0000

    First bunch of notebook tests.

---
+++ test/notebooks/nbtest_sklearn.py
@@ -0,0 +1,33 @@
+import os
+
+from notebook_test_utils import (access_to_temp_secret_store, notebook_runner)
+
+
+def test_regression(notebook_runner) -> None:
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('./data')
+        notebook_runner('data_abalone.ipynb')
+        os.chdir('../sklearn')
+        notebook_runner('sklearn_predict_udf.ipynb')
+        notebook_runner('sklearn_train_abalone.ipynb')
+        notebook_runner('sklearn_predict_abalone.ipynb')
+    finally:
+        os.chdir(current_dir)
+
+
+def test_classification(notebook_runner) -> None:
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('./data')
+        notebook_runner('data_telescope.ipynb')
+        os.chdir('../sklearn')
+        notebook_runner('sklearn_predict_udf.ipynb')
+        notebook_runner('sklearn_train_telescope.ipynb')
+        notebook_runner('sklearn_predict_telescope.ipynb')
+    finally:
+        os.chdir(current_dir)
---
+++ test/notebooks/nbtest_transformers.py
@@ -0,0 +1,49 @@
+import os
+import textwrap
+import pytest
+
+from notebook_test_utils import (access_to_temp_secret_store, notebook_runner)
+
+
+@pytest.mark.parametrize(
+    "notebook_file",
+    [
+        'sequence_classification.ipynb',
+        'question_answering.ipynb',
+        'masked_modelling.ipynb',
+        'token_classification.ipynb',
+        'text_generation.ipynb'
+        'translation.ipynb',
+        'zero_shot_classification.ipynb'
+    ]
+)
+def test_transformers(notebook_runner, notebook_file) -> None:
+
+    uploading_hack = (
+        'uploading_model',
+        textwrap.dedent("""
+            import time
+            time.sleep(20)
+        """)
+    )
+    running_hack = (
+        'running_model',
+        textwrap.dedent("""
+            errors = %sql --with udf_output SELECT error_message FROM udf_output where error_message != 'None'
+            assert not errors, str(errors)
+        """)
+    )
+    running_pyx_hack = (
+        'running_model_pyx',
+        'assert str(result["ERROR_MESSAGE"]) == "nan"'
+    )
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('./transformers')
+        notebook_runner(notebook_file='te_init.ipynb', hacks=[uploading_hack])
+        notebook_runner(notebook_file=notebook_file,
+                        hacks=[uploading_hack, running_hack, running_pyx_hack])
+    finally:
+        os.chdir(current_dir)
