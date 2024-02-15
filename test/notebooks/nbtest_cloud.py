6e5012f53cec08bbfac950102ccdfd600f5e5edc
Maxim Lapan <maxim.lapan@exasol.com>
2024-02-15 14:43:50 +0100

    Cloud storage notebook test

---
+++ test/notebooks/nbtest_cloud.py
@@ -0,0 +1,15 @@
+import os
+
+from notebook_test_utils import notebook_runner
+
+
+def test_cloud_notebook(notebook_runner) -> None:
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        notebook_runner('cloud_store_config.ipynb')
+        os.chdir('../cloud')
+        notebook_runner('01_import_data.ipynb')
+    finally:
+        os.chdir(current_dir)
