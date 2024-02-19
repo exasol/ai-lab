f1687c90d3f1ce02cfa906e18ff949cf15a024a4
Maxim Lapan <maxim.lapan@exasol.com>
2024-02-19 10:14:13 +0100

    Add pytest_plugins, fix dir name [run-notebook-tests]

---
+++ test/notebooks/conftest.py
@@ -0,0 +1,1 @@
+pytest_plugings = ["notebook_runner", "access_to_temp_secret_store"]
--- test/notebooks/nbtest_cloud.py
+++ test/notebooks/nbtest_cloud.py
@@ -1,5 +1,4 @@
 import os
-from notebook_test_utils import access_to_temp_secret_store, notebook_runner
 
 
 def test_cloud_notebook(notebook_runner) -> None:
@@ -8,7 +7,7 @@ def test_cloud_notebook(notebook_runner) -> None:
     try:
         notebook_runner('main_config.ipynb')
         notebook_runner('cloud_store_config.ipynb')
-        os.chdir('../cloud')
+        os.chdir('cloud')
         notebook_runner('01_import_data.ipynb')
     finally:
         os.chdir(current_dir)
