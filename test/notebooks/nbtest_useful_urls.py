59d18234d1cfd68af50676b946819195660ba7bf
mibe <mikhail.beck@exasol.com>
2024-03-01 08:59:18 +0000

    made useful_urls a python module

--- test/notebooks/nbtest_useful_urls.py
+++ test/notebooks/nbtest_useful_urls.py
@@ -1,29 +1,16 @@
-import nbformat
-from nbclient import NotebookClient
+import requests
+from utils.useful_urls import UsefulURLs
 
 
 def test_urls() -> None:
 
-    nb = nbformat.read('utils/useful_urls.ipynb', as_version=4)
-
-    # Add the following code at the end of the notebook
-    test_code = '''
-    def verify_all_urls():
-        import requests
-        bad_urls = []
-        for url in UsefulURLs:
-            try:
-                response = requests.head(url.value, allow_redirects=True)
-                if response.status_code >= 400:
-                    bad_urls.append(url.value)
-            except requests.RequestException:
+    bad_urls = []
+    for url in UsefulURLs:
+        try:
+            response = requests.head(url.value, allow_redirects=True)
+            if response.status_code >= 400:
                 bad_urls.append(url.value)
-        if bad_urls:
-            raise RuntimeError(f"The following URL(s) are inaccessible {bad_urls}")
-    verify_all_urls()
-    '''
-    nb.cells.append(nbformat.v4.new_code_cell(test_code))
+        except requests.RequestException:
+            bad_urls.append(url.value)
 
-    # Execute the notebook object, expecting to get no exceptions.
-    nb_client = NotebookClient(nb, kernel_name='python3')
-    nb_client.execute()
+    assert not bad_urls, f"The following URL(s) are inaccessible {bad_urls}"
