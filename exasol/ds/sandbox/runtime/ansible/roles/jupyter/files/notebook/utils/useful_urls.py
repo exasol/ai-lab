a47976be80ce6e77e4d68e9ef7c6a9b1aa5f1477
mibe <mikhail.beck@exasol.com>
2024-03-01 09:26:36 +0000

    added missed useful_urls.py

---
+++ exasol/ds/sandbox/runtime/ansible/roles/jupyter/files/notebook/utils/useful_urls.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class UsefulURLs(Enum):
+    user_manual_docker_db = 'https://github.com/exasol/ai-lab/blob/main/doc/user_guide/docker/docker-usage.md#ai-lab-with-integrated-exasol-docker-db'
