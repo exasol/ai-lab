bab2a2eed19952bfc4041215989bbf7804741016
ckunki <christoph.kuhnke@exasol.com>
2024-03-28 15:35:15 +0100

    Switched CodeBuildWaiter to use tenacity

---
+++ doc/changes/changes_2.1.0.md
@@ -0,0 +1,31 @@
+# AI-Lab 2.1.0 released TBD
+
+Code name: TBD
+
+## Summary
+
+TBD
+
+## AI-Lab-Release
+
+Version: 2.1.0
+
+## Features
+
+n/a
+
+## Security
+
+n/a
+
+## Bug Fixes
+
+n/a
+
+## Documentation
+
+n/a
+
+## Refactoring
+
+* #267: Switched CodeBuildWaiter to use tenacity
--- test/integration/aws/test_codebuild_waiter.py
+++ test/integration/aws/test_codebuild_waiter.py
@@ -1,6 +1,7 @@
 import time
 from typing import List, Iterator
 from unittest.mock import MagicMock
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -10,22 +11,16 @@ from test.mock_cast import mock_cast
 BUILD_ID = 123
 
 
-def codebuild_client(build_duration: int, finish_status: str):
+def ms(value: int) -> timedelta:
+    return timedelta(milliseconds=value)
 
-    start_time = time.time()
 
+def codebuild_client(build_duration: timedelta, finish_status: str):
+    end_time = datetime.now() + build_duration
     def batch_get_builds(ids: List[int]):
         if len(ids) == 1 and ids[0] == BUILD_ID:
-            time_elapsed = time.time() - start_time
-            if time_elapsed < build_duration:
-                return {
-                    'builds': [{'buildStatus': "IN_PROGRESS"}]
-                }
-            else:
-                return {
-                    'builds': [{'buildStatus': finish_status}]
-                }
-
+            value = finish_status if datetime.now() > end_time else "IN_PROGRESS"
+            return { 'builds': [{'buildStatus': value}] }
     codebuild_mock = MagicMock()
     mock_cast(codebuild_mock.batch_get_builds).side_effect = batch_get_builds
     return codebuild_mock
@@ -33,33 +28,41 @@ def codebuild_client(build_duration: int, finish_status: str):
 
 @pytest.mark.parametrize("interval", list(range(1, 5)))
 def test_code_build_waiter_success(interval):
-
-    codebuild_mock = codebuild_client(build_duration=5, finish_status="SUCCEEDED")
+    codebuild_mock = codebuild_client(
+        build_duration=ms(200),
+        finish_status="SUCCEEDED",
+    )
     waiter = CodeBuildWaiter(codebuild_mock, BUILD_ID)
-    waiter.wait(timeout_in_seconds=10, interval_in_seconds=interval)
+    waiter.wait(timeout=ms(300), interval=ms(interval*50))
 
 
 @pytest.mark.parametrize("interval", list(range(1, 3)))
 def test_code_build_waiter_timeout(interval):
-
-    codebuild_mock = codebuild_client(build_duration=15, finish_status="SUCCEEDED")
+    codebuild_mock = codebuild_client(
+        build_duration=ms(200),
+        finish_status="SUCCEEDED",
+    )
     waiter = CodeBuildWaiter(codebuild_mock, BUILD_ID)
     with pytest.raises(TimeoutError, match=f"Build {BUILD_ID} ran into timeout."):
-        waiter.wait(timeout_in_seconds=3, interval_in_seconds=interval)
+        waiter.wait(timeout=ms(150), interval=ms(interval*40))
 
 
 @pytest.mark.parametrize("error_status", ['FAILED', 'FAULT', 'STOPPED', 'TIMED_OUT'])
 def test_code_build_waiter_error(error_status):
-
-    codebuild_mock = codebuild_client(build_duration=3, finish_status=error_status)
+    codebuild_mock = codebuild_client(
+        build_duration=ms(50),
+        finish_status=error_status,
+    )
     waiter = CodeBuildWaiter(codebuild_mock, BUILD_ID)
     with pytest.raises(RuntimeError, match=f"Build \\({BUILD_ID}\\) failed with status: {error_status}"):
-        waiter.wait(timeout_in_seconds=10, interval_in_seconds=1)
+        waiter.wait(timeout=ms(400), interval=ms(20))
 
 
 def test_code_build_waiter_unknown_error():
-
-    codebuild_mock = codebuild_client(build_duration=3, finish_status="UNKNOWN_ERROR")
+    codebuild_mock = codebuild_client(
+        build_duration=ms(50),
+        finish_status="UNKNOWN_ERROR",
+    )
     waiter = CodeBuildWaiter(codebuild_mock, BUILD_ID)
     with pytest.raises(RuntimeError, match=f"Build {BUILD_ID} has unknown build status: UNKNOWN_ERROR"):
-        waiter.wait(timeout_in_seconds=10, interval_in_seconds=1)
+        waiter.wait(timeout=ms(100), interval=ms(20))
