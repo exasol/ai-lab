bb3608e9f99cc46f3bbb961473951130cf13853b
ckunki <christoph.kuhnke@exasol.com>
2024-08-22 08:53:02 +0200

    Whitespace

--- test/unit/aws/test_lint_cloudformation_templates.py
+++ test/unit/aws/test_lint_cloudformation_templates.py
@@ -48,6 +48,7 @@ TEMPLATES = {
 def test_lint_cloudformation_templates(tmp_path, template_key):
     validate_using_cfn_lint(tmp_path, TEMPLATES[template_key])
 
+
 @pytest.mark.parametrize("key", ("vm-bucket", "vm-bucket-waf"))
 def test_template_rendering(key):
     expected = (
