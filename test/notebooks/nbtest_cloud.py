8a68e1eaed788031d5741663e428816740515ca2
ckunki <christoph.kuhnke@exasol.com>
2024-08-23 11:39:16 +0200

    Re-enabled tests for cloud storage notebook

---
+++ test/notebooks/nbtest_cloud.py
@@ -0,0 +1,20 @@
+import os
+from notebook_test_utils import (
+    access_to_temp_secret_store,
+    notebook_runner,
+    set_log_level_for_libraries,
+)
+
+set_log_level_for_libraries()
+
+
+def test_cloud_notebook(notebook_runner) -> None:
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        notebook_runner('cloud_store_config.ipynb')
+        os.chdir('cloud')
+        notebook_runner('01_import_data.ipynb')
+    finally:
+        os.chdir(current_dir)
