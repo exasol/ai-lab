c7e4cbb624058f79dd77bf8c01a8d27a42f0b340
Torsten Kilias <torsten.kilias@exasol.com>
2024-08-23 13:49:28 +0200

    Add backend parametrize to CSE notebook test [CodeBuild]

--- test/notebooks/nbtest_cloud.py
+++ test/notebooks/nbtest_cloud.py
@@ -1,14 +1,18 @@
 import os
+
+import pytest
+
 from notebook_test_utils import (
     access_to_temp_secret_store,
     access_to_temp_saas_secret_store,
     notebook_runner,
     set_log_level_for_libraries,
 )
+from exasol.nb_connector.ai_lab_config import StorageBackend
 
 set_log_level_for_libraries()
 
-
+@pytest.mark.parametrize('notebook_runner', [StorageBackend.onprem, StorageBackend.saas], indirect=True)
 def test_cloud_notebook(notebook_runner) -> None:
 
     current_dir = os.getcwd()
