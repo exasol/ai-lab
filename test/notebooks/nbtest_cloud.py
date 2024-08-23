2e9d299050664192b56c30ccf5e15004d033dd7c
Torsten Kilias <torsten.kilias@exasol.com>
2024-08-23 17:33:45 +0200

    Fix CSE notebook test [CodeBuild]

--- test/notebooks/nbtest_cloud.py
+++ test/notebooks/nbtest_cloud.py
@@ -18,7 +18,6 @@ def test_cloud_notebook(notebook_runner) -> None:
     current_dir = os.getcwd()
     try:
         notebook_runner('main_config.ipynb')
-        notebook_runner('cloud_store_config.ipynb')
         os.chdir('cloud')
         notebook_runner('01_import_data.ipynb')
     finally:
