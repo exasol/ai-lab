748b96ea5d2f9e23eeff5eb6d731913bf2090141
Maxim Lapan <maxim.lapan@exasol.com>
2025-03-04 11:10:05 +0100

    Refactor CfTemplate

--- exasol/ds/sandbox/lib/cloudformation_templates/__init__.py
+++ exasol/ds/sandbox/lib/cloudformation_templates/__init__.py
@@ -1,5 +1,5 @@
 from .cf_template import CfTemplateSpec, CfTemplate
 from .waf import WafCfTemplate
-from .s3_buckets import S3BucketWithWAFCfTemplate
+from .s3_buckets import S3BucketWithWAFCfTemplate, S3BucketCfTemplate
 from .vm_bucket import VmBucketCfTemplate
 from .example_data_bucket import ExampleDataCfTemplate, ExampleDataS3CfTemplate
--- exasol/ds/sandbox/lib/cloudformation_templates/example_data_bucket.py
+++ exasol/ds/sandbox/lib/cloudformation_templates/example_data_bucket.py
@@ -5,6 +5,7 @@ from exasol.ds.sandbox.lib.cloudformation_templates import (
     CfTemplate,
     CfTemplateSpec,
     S3BucketWithWAFCfTemplate,
+    S3BucketCfTemplate,
 )
 from exasol.ds.sandbox.lib.config import ConfigObject
 
@@ -37,7 +38,7 @@ class ExampleDataCfTemplate(S3BucketWithWAFCfTemplate):
         super().__init__(aws_access, self.S3_BUCKET, self.WAF)
 
 
-class ExampleDataS3CfTemplate(S3BucketWithWAFCfTemplate):
+class ExampleDataS3CfTemplate(S3BucketCfTemplate):
     """
     Enables to instantiate templates for cloudformation stacks for an S3
     bucket and the related Web Application Firewall (WAF) dedicated for AI-Lab
@@ -48,19 +49,12 @@ class ExampleDataS3CfTemplate(S3BucketWithWAFCfTemplate):
         template="cloudformation/example-data-s3/s3-bucket.jinja.yaml",
         outputs={
             "BucketId": "ExampleDataBucketId",
-            "ExportRoleId": "n/a",
             "CfDistributionId": "CfDistributionId",
-            "CfDistributionDomainName": "CfDistributionDomainName",
         },
-    )
-    WAF = CfTemplateSpec(
-        cf_stack_name="AI-Lab-Example-Data-Bucket-S3-WAF",
-        template="cloudformation/example-data-s3/waf.jinja.yaml",
-        outputs={},
     )
 
     def __init__(self, aws_access: Optional[AwsAccess]):
-        super().__init__(aws_access, self.S3_BUCKET, self.WAF)
+        super().__init__(aws_access, self.S3_BUCKET)
 
     def setup(self, config: ConfigObject) -> None:
         CfTemplate.setup(self)
