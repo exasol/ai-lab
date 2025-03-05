2625192918d46728f2bc18c391b68eee7ba3e2c4
Maxim Lapan <maxim.lapan@exasol.com>
2025-03-05 11:39:18 +0100

    Fix templatization

--- exasol/ds/sandbox/templates/cloudformation/example-data-s3/lambda.py
+++ exasol/ds/sandbox/templates/cloudformation/example-data-s3/lambda.py
@@ -1,10 +1,17 @@
+
+# the first line has to be empty for jinja2 indentation ¯\_(ツ)_/¯
+import os
 import boto3
 
-s3 = boto3.client("s3")
-
+s3 = None
+ENV_BUCKET_NAME = "BUCKET_NAME"
 
 def lambda_handler(event, context):
-    bucket_name = "ai-lab-example-data-s3"
+    global s3
+    if s3 is None:
+        s3 = boto3.client("s3")
+    bucket_name = os.environ[ENV_BUCKET_NAME]
+
     # Block all public access
     s3.put_public_access_block(
         Bucket=bucket_name,
