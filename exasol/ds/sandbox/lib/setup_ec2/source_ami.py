1188f00e80b8a1c209b657888c25201243bbb8a0
ckunki <christoph.kuhnke@exasol.com>
2025-08-20 10:45:03 +0200

    Fixed review findings.

--- exasol/ds/sandbox/lib/setup_ec2/source_ami.py
+++ exasol/ds/sandbox/lib/setup_ec2/source_ami.py
@@ -1,25 +1,27 @@
 from datetime import datetime, timezone
-from dataclasses import dataclass
 
 from exasol.ds.sandbox.lib.aws_access.ami import Ami
 from exasol.ds.sandbox.lib.aws_access.aws_access import AwsAccess
 
 
 class FindAmiError(Exception):
-    ...
+    """
+    In case the specified filters did not match any AMI or
+    when selecing an AMI by ID the ID was not unique.
+    """
 
 
-@dataclass
 class AmiFinder:
-    aws_access: AwsAccess
-    default_filters: dict[str, str]
+    def __init__(self, aws_access: AwsAccess, filters: dict[str, str]):
+        self._aws_access = aws_access
+        self._default_filters = filters
 
     def _list(self, filters: dict[str, str]) -> list[Ami]:
         filter_list = [
             {"Name": key, "Values": [value]}
             for key, value in filters.items()
         ]
-        return self.aws_access.list_amis(filters=filter_list)
+        return self._aws_access.list_amis(filters=filter_list)
 
     def unique(self, filters: dict[str, str]) -> Ami:
         amis = self._list(filters)
@@ -35,9 +37,9 @@ class AmiFinder:
                 return datetime.fromisoformat(value[:-1]).replace(tzinfo=timezone.utc)
             return datetime.fromisoformat(value)
 
-        amis = self._list(self.default_filters)
+        amis = self._list(self._default_filters)
         if len(amis) < 1:
-            raise FindAmiError(f"Couldn't find any AMI matching: {self.default_filters}")
+            raise FindAmiError(f"Couldn't find any AMI matching: {self._default_filters}")
         latest = max(amis, key=lambda ami: as_datetime(ami.creation_date))
         return latest
 
