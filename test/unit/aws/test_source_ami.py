1fa704c95c678f2897bb07c7f71201823dd8b063
ckunki <christoph.kuhnke@exasol.com>
2025-08-22 09:35:30 +0200

    Fixed review findings 4

--- test/unit/aws/test_source_ami.py
+++ test/unit/aws/test_source_ami.py
@@ -1,5 +1,4 @@
-import re
-from typing import NamedTuple, Tuple
+from typing import NamedTuple
 from unittest.mock import Mock
 
 import pytest
@@ -12,21 +11,7 @@ class AmiSpec(NamedTuple):
     id: str
 
 
-# The following is based on a dump of data returned from aws cli:
-# aws ec2 --profile exa_individual_mfa describe-images
-# --owners 099720109477
-# --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
-
-AMI_SPECS = [
-    AmiSpec("2020-10-14", "ami-056114420b6ed624e"),
-    AmiSpec("2022-03-31", "ami-0d672276deff62a7b"),
-    AmiSpec("2022-06-28", "ami-0d203747b007677da"),
-    AmiSpec("2022-06-10", "ami-0c9354388bb36c088"),
-]
-
-
-@pytest.fixture(scope="session")
-def sample_amis():
+def aws_mock(specs: list[AmiSpec]) -> Mock:
     def ami(spec: AmiSpec):
         suffix = spec.date.replace("-", "")
         return Ami({
@@ -38,58 +23,63 @@ def sample_amis():
             "Name": f"ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-{suffix}",
         })
 
-    return [ami(spec) for spec in AMI_SPECS]
-
-
-@pytest.fixture(scope="session")
-def aws_mock(sample_amis):
-    def mock_list_amis(filters: list[dict[str, str | list[str]]]) -> list[Ami]:
-        def matches(ami: Ami) -> bool:
-            for f in filters:
-                key = f["Name"]
-                att = key.title().replace("-", "")
-                for crit in f["Values"]:
-                    regex = crit.replace("*", ".*")
-                    value = ami._aws_object[att]
-                    if not re.match(regex, value):
-                        return False
-            return True
-
-        return [ami for ami in sample_amis if matches(ami)]
-
-    return Mock(list_amis=mock_list_amis)
+    aws = Mock()
+    aws.list_amis.return_value = [ami(spec) for spec in specs]
+    return aws
 
 
 @pytest.fixture
-def sample_finder(aws_mock, test_config) -> AmiFinder:
-    return AmiFinder(aws_mock, test_config.source_ami_filters)
+def finder_without_amis(test_config) -> AmiFinder:
+    return AmiFinder(aws_mock([]), test_config.source_ami_filters)
 
 
-def test_unique(sample_finder):
-    id = "ami-0c9354388bb36c088"
-    ami = sample_finder.find(id)
-    assert ami.id == id
+def test_unique_success(test_config):
+    spec = AmiSpec("2022-06-10", "ami-0c9354388bb36c088")
+    testee = AmiFinder(aws_mock([spec]), test_config.source_ami_filters)
+    ami = testee.find(spec.id)
+    assert ami.id == spec.id
 
 
-def test_unique_failure(sample_finder):
-    with pytest.raises(FindAmiError):
-        sample_finder.find("non-existing-id")
+def test_unique_no_ami_found(finder_without_amis):
+    with pytest.raises(FindAmiError, match="Couldn't find any AMI"):
+        finder_without_amis.find("non-existing-id")
 
 
-def test_latest(sample_finder):
+def test_unique_multiple_amis_found(test_config):
+    duplicate_id = "ami-0d672276deff62a7b"
+    specs = [
+        AmiSpec("2020-10-14", duplicate_id),
+        AmiSpec("2022-03-31", duplicate_id),
+    ]
+    aws = aws_mock(specs)
+    testee = AmiFinder(aws, test_config.source_ami_filters)
+    with pytest.raises(FindAmiError, match="Found more than one"):
+        testee.find(duplicate_id)
+
+
+def test_latest_success(test_config):
     """
     Test that AmiFinder.latest() returns the latest AMI image based on the
     filters given in the global config.
+
+    The list is based on a dump of data returned from aws cli:
+    aws ec2 --profile exa_individual_mfa describe-images \
+      --owners 099720109477 \
+      --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
     """
-    ami = sample_finder.find(None)
-    # ami-0d203747b007677da is the latest one in the mock data
-    assert ami.id == "ami-0d203747b007677da"
+    latest_ami = AmiSpec("2022-06-28", "ami-0d203747b007677da")
+    specs = [
+        AmiSpec("2020-10-14", "ami-056114420b6ed624e"),
+        AmiSpec("2022-03-31", "ami-0d672276deff62a7b"),
+        latest_ami,
+        AmiSpec("2022-06-10", "ami-0c9354388bb36c088"),
+    ]
+    aws = aws_mock(specs)
+    testee = AmiFinder(aws, test_config.source_ami_filters)
+    ami = testee.find(None)
+    assert ami.id == latest_ami.id
 
 
-def test_latest_failure(aws_mock):
-    filters = {
-        "name": "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*",
-    }
-    finder = AmiFinder(aws_mock, filters)
+def test_latest_failure(finder_without_amis):
     with pytest.raises(FindAmiError):
-        finder.find(None)
+        finder_without_amis.find(None)
