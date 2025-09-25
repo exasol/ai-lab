44cd82cab6b292949d78c16dd6c161f69cad4fe1
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-25 10:58:37 -0300

    Added test

--- test/notebooks/nbtest_transformers.py
+++ test/notebooks/nbtest_transformers.py
@@ -24,7 +24,8 @@ set_log_level_for_libraries()
         'token_classification.ipynb',
         'text_generation.ipynb',
         'translation.ipynb',
-        'zero_shot_classification.ipynb'
+        'zero_shot_classification.ipynb',
+
     ]
 )
 def test_transformers(notebook_runner, uploading_hack, notebook_file) -> None:
@@ -48,5 +49,38 @@ def test_transformers(notebook_runner, uploading_hack, notebook_file) -> None:
         notebook_runner(notebook_file='te_init.ipynb', hacks=[uploading_hack])
         notebook_runner(notebook_file=notebook_file,
                         hacks=[uploading_hack, running_hack, running_pyexasol_hack])
+    finally:
+        os.chdir(current_dir)
+
+def test_model_mgmt(notebook_runner, uploading_hack) -> None:
+    notebook_file = "model_administration.ipynb"
+
+    running_hack = (
+        'running_model',
+        textwrap.dedent("""
+            errors = %sql --with udf_output SELECT error_message FROM udf_output WHERE error_message != 'None'
+            assert not errors, str(errors)
+        """)
+    )
+    running_error_hack = (
+        'running_model_error',
+        textwrap.dedent("""
+            errors = %sql --with udf_output_error SELECT error_message FROM udf_output_error WHERE error_message != 'None'
+            assert len(errors) == 1
+            assert "No such file or directory:" in str(errors[0])
+        """)
+    )
+    running_pyexasol_hack = (
+        'running_model_pyexasol',
+        'assert str(result["ERROR_MESSAGE"]) == "nan"'
+    )
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('./transformers')
+        notebook_runner(notebook_file='te_init.ipynb', hacks=[uploading_hack])
+        notebook_runner(notebook_file=notebook_file,
+                        hacks=[uploading_hack, running_hack, running_error_hack, running_pyexasol_hack])
     finally:
         os.chdir(current_dir)
