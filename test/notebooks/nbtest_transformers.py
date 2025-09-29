6551a2dcac224c0a4d5c94b44012a111b26a6855
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-29 07:22:36 -0300

    Fixes from review

--- test/notebooks/nbtest_transformers.py
+++ test/notebooks/nbtest_transformers.py
@@ -8,39 +8,58 @@ from notebook_test_utils import (
     backend_setup,
     notebook_runner,
     uploading_hack,
-    set_log_level_for_libraries, print_notebook_output,
+    set_log_level_for_libraries,
 )
 
 
 set_log_level_for_libraries()
 
+running_hack = (
+    'running_model',
+    textwrap.dedent("""
+        errors = %sql --with udf_output SELECT error_message FROM udf_output WHERE error_message != 'None'
+        assert not errors, str(errors)
+    """)
+)
+
+running_delete_hack = (
+    'running_model_delete',
+    textwrap.dedent("""
+        df = model_delete.DataFrame()
+        assert df.loc[0, 'success'], df.loc[0, 'success']
+        assert not df.loc[0, 'error_message'], df.loc[0, 'error_message']
+    """)
+)
+
+running_delete_error_hack = (
+    'running_model_delete_error',
+    textwrap.dedent("""
+        df = model_delete_with_error.DataFrame()
+        assert not df.loc[0, 'success'], df.loc[0, 'success']
+        assert "No such file or directory:" in df.loc[0, 'error_message'], df.loc[0, 'error_message']
+    """)
+)
+
+running_pyexasol_hack = (
+    'running_model_pyexasol',
+    'assert str(result["ERROR_MESSAGE"]) == "nan"'
+)
+
 
 @pytest.mark.parametrize(
-    "notebook_file",
+    "notebook_file, hacks",
     [
-        'sequence_classification.ipynb',
-        'question_answering.ipynb',
-        'masked_modelling.ipynb',
-        'token_classification.ipynb',
-        'text_generation.ipynb',
-        'translation.ipynb',
-        'zero_shot_classification.ipynb',
-
+        ('sequence_classification.ipynb',[running_hack, running_pyexasol_hack]),
+        ('question_answering.ipynb',[running_hack, running_pyexasol_hack]),
+        ('masked_modelling.ipynb',[running_hack, running_pyexasol_hack]),
+        ('token_classification.ipynb',[running_hack, running_pyexasol_hack]),
+        ('text_generation.ipynb',[running_hack, running_pyexasol_hack]),
+        ('translation.ipynb',[running_hack, running_pyexasol_hack]),
+        ('zero_shot_classification.ipynb',[running_hack, running_pyexasol_hack]),
+        ("model_management.ipynb", [running_hack, running_delete_hack, running_pyexasol_hack, running_delete_error_hack])
     ]
 )
-def test_transformers(notebook_runner, uploading_hack, notebook_file) -> None:
-
-    running_hack = (
-        'running_model',
-        textwrap.dedent("""
-            errors = %sql --with udf_output SELECT error_message FROM udf_output WHERE error_message != 'None'
-            assert not errors, str(errors)
-        """)
-    )
-    running_pyexasol_hack = (
-        'running_model_pyexasol',
-        'assert str(result["ERROR_MESSAGE"]) == "nan"'
-    )
+def test_transformers(notebook_runner, uploading_hack, notebook_file, hacks) -> None:
 
     current_dir = os.getcwd()
     try:
@@ -48,47 +67,6 @@ def test_transformers(notebook_runner, uploading_hack, notebook_file) -> None:
         os.chdir('./transformers')
         notebook_runner(notebook_file='te_init.ipynb', hacks=[uploading_hack])
         notebook_runner(notebook_file=notebook_file,
-                        hacks=[uploading_hack, running_hack, running_pyexasol_hack])
-    finally:
-        os.chdir(current_dir)
-
-def test_model_mgmt(notebook_runner, uploading_hack) -> None:
-    notebook_file = "model_management.ipynb"
-
-    running_hack = (
-        'running_model',
-        textwrap.dedent("""
-            errors = %sql --with udf_output SELECT error_message FROM udf_output WHERE error_message != 'None'
-            assert not errors, str(errors)
-        """)
-    )
-    running_delete_hack = (
-        'running_model_delete',
-        textwrap.dedent("""
-            df = model_delete.DataFrame()
-            assert df.loc[0, 'success'], df.loc[0, 'success']
-            assert not df.loc[0, 'error_message'], df.loc[0, 'error_message']
-        """)
-    )
-    running_delete_error_hack = (
-        'running_model_delete_error',
-        textwrap.dedent("""
-            df = model_delete_with_error.DataFrame()
-            assert not df.loc[0, 'success'], df.loc[0, 'success']
-            assert "No such file or directory:" in df.loc[0, 'error_message'], df.loc[0, 'error_message']
-        """)
-    )
-    running_pyexasol_hack = (
-        'running_model_pyexasol',
-        'assert str(result["ERROR_MESSAGE"]) == "nan"'
-    )
-
-    current_dir = os.getcwd()
-    try:
-        notebook_runner('main_config.ipynb')
-        os.chdir('./transformers')
-        notebook_runner(notebook_file='te_init.ipynb', hacks=[uploading_hack])
-        notebook_runner(notebook_file=notebook_file,
-                        hacks=[uploading_hack, running_hack, running_delete_hack, running_delete_error_hack, running_pyexasol_hack])
+                        hacks=hacks)
     finally:
         os.chdir(current_dir)
