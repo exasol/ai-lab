a2706ca7ec52e3223d3ef460ab0aadef77def5c7
ckunki <christoph.kuhnke@exasol.com>
2025-10-20 15:43:40 +0200

    Added integration test

---
+++ test/notebooks/nbtest_first_steps.py
@@ -0,0 +1,13 @@
+# We need to manually import all fixtures that we use, directly or indirectly,
+# since the pytest won't do this for us.
+from notebook_test_utils import (
+    notebook_runner,
+    set_log_level_for_libraries,
+)
+
+set_log_level_for_libraries()
+
+
+def test_first_steps_notebook(notebook_runner) -> None:
+    notebook_runner('main_config.ipynb')
+    notebook_runner('first_steps.ipynb')
