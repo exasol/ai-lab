88738f331d6be4f2c6dd6d68786733981435b9b9
ckunki <christoph.kuhnke@exasol.com>
2025-10-21 11:43:40 +0200

    Fixed integration test

--- test/notebooks/nbtest_first_steps.py
+++ test/notebooks/nbtest_first_steps.py
@@ -1,6 +1,7 @@
 # We need to manually import all fixtures that we use, directly or indirectly,
 # since the pytest won't do this for us.
 from notebook_test_utils import (
+    backend_setup,
     notebook_runner,
     set_log_level_for_libraries,
 )
