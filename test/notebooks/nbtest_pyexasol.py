0083c048863c7263f09197e053ec1288db78f8f7
Ariel Schulz <ariel.schulz@exasol.com>
2025-12-03 12:55:17 +0100

    Add pass through test

---
+++ test/notebooks/nbtest_pyexasol.py
@@ -0,0 +1,23 @@
+import os
+import textwrap
+
+# We need to manually import all fixtures that we use, directly or indirectly,
+# since the pytest won't do this for us.
+from notebook_test_utils import (
+    backend_setup,
+    notebook_runner,
+    set_log_level_for_libraries,
+)
+
+
+set_log_level_for_libraries()
+
+
+def test_roundtrip_import_and_export(notebook_runner) -> None:
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('../pyexasol')
+        notebook_runner('roundtrip_import_and_export.ipynb')
+    finally:
+        os.chdir(current_dir)
