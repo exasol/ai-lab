7b879e2351cd8068a0b419f2d1002a3119021c0c
ckunki <christoph.kuhnke@exasol.com>
2025-12-11 16:46:13 +0100

    Fixed test_dss_docker_image

--- test/unit/test_dss_docker_image.py
+++ test/unit/test_dss_docker_image.py
@@ -5,6 +5,7 @@ from unittest.mock import MagicMock, Mock, create_autospec, patch
 import pytest
 
 from exasol.ds.sandbox.lib.ansible.ansible_run_context import AnsibleRunContext
+from exasol.ds.sandbox.lib.ansible.facts import AnsibleFacts
 from exasol.ds.sandbox.lib.config import AI_LAB_VERSION
 from exasol.ds.sandbox.lib.dss_docker import create_image, DockerRegistry
 from exasol.ds.sandbox.lib.dss_docker.create_image import DssDockerImage
@@ -35,58 +36,14 @@ def test_constructor(sample_repo):
     assert testee.work_in_progress_notebooks == True
 
 
-# @pytest.mark.parametrize(
-#     "testee",
-#     [
-#         {},
-#         {"key": "sample value"},
-#     ])
-# def test_nested_value_missing_entry(testee):
-#     assert create_image.get_nested_value(testee, "missing_entry") == None
-#     assert create_image.get_nested_value(testee, "key", "key-2") == None
-#
-#
-# def test_nested_value_level_2():
-#     testee = {"key": {"key-2": "value"}}
-#     assert create_image.get_nested_value(testee, "key", "key-2") == "value"
-#
-#
-# @pytest.mark.parametrize(
-#     "facts",
-#     [
-#         {},
-#         {"other": "other value"},
-#         {"dss_facts": {}},
-#         {"dss_facts": {"b": {}}},
-#     ])
-# def test_fact_empty(facts):
-#     assert create_image.get_fact(facts, "a", "b", "c") is None
-#
-#
-# def test_fact_found():
-#     facts = {"dss_facts": {"b": {"c": "expected"}}}
-#     assert create_image.get_fact(facts, "b", "c") == "expected"
-#
-#
-# @pytest.mark.parametrize(
-#     "facts",
-#     [
-#         {},
-#         {"key": "value"},
-#         {"dss_facts": {}},
-#         {"dss_facts": {"key": "value"}},
-#     ])
-# def test_entrypoint_default(facts):
-#     assert create_image.entrypoint(facts) == ["sleep", "infinity"]
-
-
 def test_entrypoint_with_copy_args():
-    facts = {
+    raw_facts = {
         "dss_facts": {
             "docker_group": "docker-group-name",
             "jupyter": {
                 "virtualenv": "/home/jupyter/jupyterenv",
-                "command": "/home/jupyter/jupyterenv/bin/jupyter-lab",
+                "server": "/home/jupyter/jupyterenv/bin/jupyter-lab",
+                "core": "/home/jupyter/jupyterenv/bin/jupyter",
                 "port": "port",
                 "user": "jupyter-user-name",
                 "group": "jupyter-group-name",
@@ -99,40 +56,52 @@ def test_entrypoint_with_copy_args():
                 "initial": "/path/to/initial",
                 "final": "/path/to/final",
             }}}
+    facts = AnsibleFacts(raw_facts)
 
     def fact(*args):
-        return create_image.get_fact(facts, *args)
+        return facts.get(*args)
+        #return create_image.get_fact(facts, *args)
 
     expected = {
-        "--docker-group": fact("docker_group"),
-        "--notebook-defaults": fact("notebook_folder", "initial"),
-        "--notebooks": fact("notebook_folder", "final"),
-        "--home": fact("jupyter", "home"),
-        "--venv": fact("jupyter", "virtualenv"),
-        "--jupyter-server": fact("jupyter", "command"),
-        "--port": fact("jupyter", "port"),
-        "--user": fact("jupyter", "user"),
-        "--group": fact("jupyter", "group"),
-        "--password": fact("jupyter", "password"),
-        "--jupyter-logfile": fact("jupyter", "logfile"),
+        "positional": [
+            "sudo",
+            "--preserve-env=JUPYTER_PASSWORD",
+            "python3",
+            fact("entrypoint"),
+        ],
+        "options": {
+            "--docker-group": fact("docker_group"),
+            "--notebook-defaults": fact("notebook_folder", "initial"),
+            "--notebooks": fact("notebook_folder", "final"),
+            "--home": fact("jupyter", "home"),
+            "--venv": fact("jupyter", "virtualenv"),
+            "--jupyter-server": fact("jupyter", "server"),
+            "--jupyter-core": fact("jupyter", "core"),
+            "--port": fact("jupyter", "port"),
+            "--user": fact("jupyter", "user"),
+            "--group": fact("jupyter", "group"),
+            "--password": fact("jupyter", "password"),
+            "--jupyter-logfile": fact("jupyter", "logfile"),
+        }
     }
-    actual = create_image.entrypoint(facts)
+    actual = create_image.build_entrypoint(facts)
 
     def as_dict(lst: List[str]) -> Dict[str, str]:
         return {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
 
-    assert ["sudo", "python3", fact("entrypoint")] == actual[:3] \
-           and expected == as_dict(actual[3:])
+    n = len(expected["positional"])
+    assert expected["positional"] == actual[:n]
+    assert expected["options"] == as_dict(actual[n:])
 
 
-def testee_to_mock() -> DssDockerImage:
+def create_testee() -> DssDockerImage:
     testee = DssDockerImage("org/sample_repo", "version")
     return testee
 
 
 @pytest.fixture
 def mocked_docker_image() -> DssDockerImage:
-    testee = testee_to_mock()
+    testee = create_testee()
     testee._start = datetime.now()
     testee._start_container = MagicMock()
     testee._install_dependencies = MagicMock()
@@ -158,7 +127,7 @@ def test_push_called(mocker, mocked_docker_image):
 def test_work_in_progress_notebooks(mocked_run_install_dependencies: Mock,
                                     mocked_docker_image: DssDockerImage):
     testee = mocked_docker_image
-    testee._install_dependencies = testee_to_mock()._install_dependencies
+    testee._install_dependencies = create_testee()._install_dependencies
     testee.create()
     assert len(mocked_run_install_dependencies.mock_calls) == 1
     ansible_run_context = mocked_run_install_dependencies.mock_calls[0].kwargs["ansible_run_context"]
