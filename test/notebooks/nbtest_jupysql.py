55f85050805f44bfe53686727dd2dee629382a02
Ariel Schulz <ariel.schulz@exasol.com>
2026-03-17 12:19:48 +0100

    Run data_flights.ipynb first

--- test/notebooks/nbtest_jupysql.py
+++ test/notebooks/nbtest_jupysql.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 
 # We need to manually import all fixtures that we use, directly or indirectly,
 # since the pytest won't do this for us.
@@ -20,10 +21,20 @@ def test_quickstart(notebook_runner, monkeypatch) -> None:
     monkeypatch.setenv("EXASOL_SCHEMA", "AI_LAB")
     monkeypatch.setenv("EXASOL_SSL_CERTIFICATE", "SSL_VERIFY_NONE")
 
+    data_import_hack = (
+        'data_selection',
+        textwrap.dedent("""
+            load_flights_data(ai_lab_config, ['Jan 2024'])
+            load_airlines_data(ai_lab_config)
+        """)
+    )
+
     current_dir = os.getcwd()
     try:
         notebook_runner('main_config.ipynb')
-        os.chdir('./jupysql')
+        os.chdir('./data')
+        notebook_runner('data_flights.ipynb', hacks=[data_import_hack])
+        os.chdir('../jupysql')
         notebook_runner('quickstart.ipynb')
     finally:
         os.chdir(current_dir)
