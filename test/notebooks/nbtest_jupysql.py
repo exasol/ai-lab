1af6480dca339738f559a3c882b04ae3db55f52f
Ariel Schulz <43442541+ArBridgeman@users.noreply.github.com>
2026-03-17 19:34:27 +0100

    Feature/480 add jupysql starter notebook (#481)
    * Add initial notebook
    * Improve initial notebook
    * Update apt get git and sudo dependencies
    * Switch to state_abr, as state is a reserved Exasol word
    * Add verification test and fix one for PyExasol notebook
    * Update alternate method to be secure
    * Alphabetize nbtest file

---
+++ test/notebooks/nbtest_jupysql.py
@@ -0,0 +1,48 @@
+import os
+import textwrap
+
+from exasol.nb_connector.secret_store import Secrets
+
+# We need to manually import all fixtures that we use, directly or indirectly,
+# since the pytest won't do this for us.
+from notebook_test_utils import (
+    backend_setup,
+    notebook_runner,
+    set_log_level_for_libraries,
+)
+
+set_log_level_for_libraries()
+
+
+def test_quickstart(notebook_runner, monkeypatch, backend_setup) -> None:
+    store_path, store_password = backend_setup
+    secrets = Secrets(store_path, master_password=store_password)
+
+    monkeypatch.setenv("EXASOL_HOST", secrets.db_host_name)
+    monkeypatch.setenv("EXASOL_PORT", secrets.db_port)
+    monkeypatch.setenv("EXASOL_USER", secrets.db_user)
+    monkeypatch.setenv("EXASOL_PASSWORD", secrets.db_password)
+    monkeypatch.setenv("EXASOL_SCHEMA", secrets.db_schema)
+    monkeypatch.setenv("EXASOL_SSL_CERTIFICATE", "SSL_VERIFY_NONE")
+
+    # load_flights_data() is defined in file flight_utils.ipynb.
+    # The function accepts a list of months used as suffixes for CSV files to import.
+    # The CSV files have been uploaded to cloudfront in advance.
+    # The URL is defined in file flight_utils.ipynb as well.
+    data_import_hack = (
+        'data_selection',
+        textwrap.dedent("""
+            load_flights_data(ai_lab_config, ['Jan 2024'])
+            load_airlines_data(ai_lab_config)
+        """)
+    )
+
+    current_dir = os.getcwd()
+    try:
+        notebook_runner('main_config.ipynb')
+        os.chdir('./data')
+        notebook_runner('data_flights.ipynb', hacks=[data_import_hack])
+        os.chdir('../jupysql')
+        notebook_runner('quickstart.ipynb')
+    finally:
+        os.chdir(current_dir)
--- test/notebooks/nbtest_pyexasol.py
+++ test/notebooks/nbtest_pyexasol.py
@@ -17,7 +17,7 @@ def test_roundtrip_import_and_export(notebook_runner) -> None:
     current_dir = os.getcwd()
     try:
         notebook_runner('main_config.ipynb')
-        os.chdir('../pyexasol')
+        os.chdir('./pyexasol')
         notebook_runner('roundtrip_import_and_export.ipynb')
     finally:
         os.chdir(current_dir)
