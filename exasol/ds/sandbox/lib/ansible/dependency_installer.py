d58913e5c94df5f1063ed4aebb30ce7772f251ad
Janaarthanan Selvarajan <janaarthanan.selvarajan@exasol.com>
2026-04-22 14:14:02 +0530

    update apt_dependency to ubuntu2.4 (#495)
    Updated apt_dependencies to ubuntu2.4 
    Updated version of juypterlab from 4.2.5 to 4.4.8 in file jupyter_requirements.txt
    Limited PEC version to < 0.14

--- exasol/ds/sandbox/lib/ansible/dependency_installer.py
+++ exasol/ds/sandbox/lib/ansible/dependency_installer.py
@@ -1,14 +1,21 @@
-from dataclasses import dataclass
-
-from exasol.ds.sandbox.lib.ansible.ansible_repository import (
-    AnsibleRepository, default_repositories)
-from exasol.ds.sandbox.lib.ansible.ansible_run_context import (
-    AnsibleRunContext, default_ansible_run_context)
+from dataclasses import (
+    dataclass,
+    field,
+)
 
 from exasol.ds.sandbox.lib.ansible.ansible_access import AnsibleAccess
+from exasol.ds.sandbox.lib.ansible.ansible_repository import (
+    AnsibleRepository,
+    default_repositories,
+)
+from exasol.ds.sandbox.lib.ansible.ansible_run_context import (
+    AnsibleRunContext,
+    default_ansible_run_context,
+)
+
 
 @dataclass
 class AnsibleDependencyInstaller:
     ansible_access: AnsibleAccess
-    run_context: AnsibleRunContext = default_ansible_run_context
+    run_context: AnsibleRunContext = field(default_factory=lambda: default_ansible_run_context)
     repositories: tuple[AnsibleRepository, ...] = default_repositories
